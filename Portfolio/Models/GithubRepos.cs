28d173b8162a732cca0a78cf94e380a756386e19
Rakhee Gandhi <rpgandhi@gmail.com>
2018-01-31 13:21:20 -0800

    Fix API functionality.

--- Portfolio/Models/GithubRepos.cs
+++ Portfolio/Models/GithubRepos.cs
@@ -20,11 +20,18 @@ namespace Portfolio.Models
         public static List<GithubRepos> GetStarredRepos()
         {
 
-            var client = new RestClient("https://api.github.com");
-            var request = new RestRequest("users/rpgandhi/starred", Method.GET);
+            var client = new RestClient("https://api.github.com");           
+            var request = new RestRequest("users/rpgandhi/starred", Method.GET);       
+            //var request = new RestRequest("/search/users?q=rpgandhi&sort=stars&order=asc", Method.GET);   
+            //var request = new RestRequest("/search/repositories", Method.GET); 
 
             request.AddHeader("Accept", "application/vnd.github.v3.full+json");
-            request.AddParameter("order", "asc");
+            request.AddHeader("User-Agent", "");           
+            //request.AddParameter("order", "asc");
+            //request.AddParameter("q", "user:rpgandi");    
+            //request.AddParameter("sort", "stars");
+            request.AddParameter("order", "asc");                 
+
 
             var response = new RestResponse();
 
@@ -36,7 +43,8 @@ namespace Portfolio.Models
             JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(response.Content);
             List<GithubRepos> projectList = JsonConvert.DeserializeObject<List<GithubRepos>>(jsonResponse.ToString());
             Console.WriteLine(projectList);
-            return projectList;
+            List<GithubRepos> top3Repos = projectList.Take(3).ToList();
+            return top3Repos;     
         }
 
         public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
